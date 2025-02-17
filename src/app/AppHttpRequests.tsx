06856213cdb7b5e43a71c1f803ccd3d81e6e2428
roma1524 <kulikr.js@mail.ru>
2025-02-17 23:14:35 +0300

    added get and post request

--- src/app/AppHttpRequests.tsx
+++ src/app/AppHttpRequests.tsx
@@ -2,16 +2,36 @@ import {type ChangeEvent, type CSSProperties, useEffect, useState} from 'react'
 import Checkbox from '@mui/material/Checkbox'
 import {CreateItemForm} from '@/common/components/CreateItemForm/CreateItemForm'
 import {EditableSpan} from '@/common/components/EditableSpan/EditableSpan'
+import axios from "axios";
+
+const token = '28d502e6-28f6-4c8f-9eae-870db6caf5f3';
+const apiKey = 'bfc4c0d3-dfa3-4627-88d9-e41dc38d4cc4';
 
 export const AppHttpRequests = () => {
-  const [todolists, setTodolists] = useState<any>([])
+  const [todolists, setTodolists] = useState<Todolist[]>([])
   const [tasks, setTasks] = useState<any>({})
 
   useEffect(() => {
-    // get todolists
+    axios.get<Todolist[]>(`https://social-network.samuraijs.com/api/1.1/todo-lists`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      }
+    })
+        .then(res => {
+          setTodolists(res.data)})
   }, [])
 
-  const createTodolist = (title: string) => {}
+  const createTodolist = (title: string) => {
+    axios.post<CreateTodoResponse>(`https://social-network.samuraijs.com/api/1.1/todo-lists`, {title}, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'API-KEY': apiKey
+      }
+    })
+        .then(res => {
+          const newTodo = res.data.data.item
+          setTodolists([newTodo, ...todolists])})
+  }
 
   const deleteTodolist = (id: string) => {}
 
@@ -28,7 +48,7 @@ export const AppHttpRequests = () => {
   return (
       <div style={{margin: '20px'}}>
         <CreateItemForm onCreateItem={createTodolist}/>
-        {todolists.map((todolist: any) => (
+        {todolists.map((todolist) => (
             <div key={todolist.id} style={container}>
               <div>
                 <EditableSpan value={todolist.title}
@@ -59,4 +79,23 @@ const container: CSSProperties = {
   display: 'flex',
   justifyContent: 'space-between',
   flexDirection: 'column',
+}
+
+export type Todolist = {
+  id: string
+  addedDate: string
+  order: number
+  title: string
+}
+
+export type FieldError = {
+  error: string
+  field: string
+}
+
+export type CreateTodoResponse = {
+  data: {item: Todolist}
+  fieldsErrors: FieldError[]
+  messages: string[]
+  resultCode: number
 }
